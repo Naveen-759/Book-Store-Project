707a672eeb4f47cc1ea3dcd9d83d9fe258a39a23
Naveen-759 <naveenku759@gmail.com>
2024-12-02 21:37:49 +0530

    first-commit

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -51,10 +51,10 @@ const ShowBook = () => {
             <span className="text-xl mr-4 text-gray-500">Create Time</span>
             <span>{new Date(book.createdAt).toString()}</span>
           </div>
-          <div className="my-4">
+          {/* <div className="my-4">
             <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
             <span>{new Date(book.updateAt).toString()}</span>
-          </div>
+          </div> */}
         </div>
       )}
     </div>
